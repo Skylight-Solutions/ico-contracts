98a7905113888ab6ef1a64b83f1cd83e0e55a586
Rainer Körner <rainer.koerner@gmail.com>
2021-12-04 11:11:27 +0100

    added clct burn test

--- ethereum/ico/test/CollectCoin.js
+++ ethereum/ico/test/CollectCoin.js
@@ -1,7 +1,37 @@
 const CollectCoin = artifacts.require("CollectCoin");
 
+const truffleAssert = require('truffle-assertions');
+
 contract("CollectCoin", accounts => {
-    it("should be ok", () => {
-        assert.equal(1,1);
+
+    let clct
+
+    before(async () => {
+        clct = await CollectCoin.deployed()
+
+        await clct.transfer(accounts[1], "100000")
+        await clct.transfer(accounts[2], "100000")
+    });
+
+    it("should not be possible to burn more tokens than caller owns", async () => {
+        const burnFunc = async () => clct.burn("100001", { from: accounts[1] });
+
+        await truffleAssert.reverts(burnFunc(), "ERC20: burn amount exceeds balance");
+    });
+
+    it("should be able to burn own tokens", async () => {
+
+        const burn_amount = "10000"
+        const balance_before = await clct.balanceOf(accounts[0])
+    
+        await clct.burn(burn_amount)
+
+        const balance_after = await clct.balanceOf(accounts[0])
+
+        const diff = balance_before.sub(balance_after)
+
+        console.log("Burned amount", diff.toString())
+
+        assert(diff.toString() == burn_amount, "burned amount is not expected: " + diff.toString());
     });
 })
